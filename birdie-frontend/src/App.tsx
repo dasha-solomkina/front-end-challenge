d891e2f858d94b12e6d1327c4e572fe3104518ed
darialiit <solomkina.daria@gmail.com>
2024-05-26 20:34:34 -0300

    prevent rerenders

--- birdie-frontend/src/App.tsx
+++ birdie-frontend/src/App.tsx
@@ -1,32 +1,43 @@
+import { useState, useEffect, useCallback } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import fetchFeedbackList, {
+  FeedbackListProps,
+} from '../src/store/dataFeedback.ts';
+import { COLORS, getRandomColor } from './store/colors.ts';
+
 import Chart from './components/Chart';
 import Search from './components/Search';
 import FeedbackList from './components/FeedbackList';
 import Pagination from './components/Pagination';
-import { useState, useEffect } from 'react';
-import fetchFeedbackList, {
-  FeedbackListProps,
-} from '../src/store/dataFeedback.ts';
-import { v4 as uuidv4 } from 'uuid';
 import FeedbackPerPage from './components/FeedbckPerPage.tsx';
 import AddTagBtn from './components/AddTagBtn.tsx';
 import TagsList from './components/TagsList.tsx';
 import { TagProp } from './components/Tag.tsx';
-import { COLORS, getRandomColor } from './store/colors.ts';
 
 function App() {
   const [search, setSearch] = useState<string>('');
   const [pageNumber, setPageNumber] = useState<number>(0);
   const [pageSize, setPageSize] = useState<number>(10);
+  const [fetchedFeedback, setFetchedFeedback] =
+    useState<FeedbackListProps | null>(null);
+  const [tags, setTags] = useState<TagProp[]>([]);
+  const [buttonVisible, setButtonVisible] = useState<boolean>(false);
+  const [buttonPosition, setButtonPosition] = useState<{
+    top: number;
+    left: number;
+  }>({ top: 0, left: 0 });
+  const [highlightedText, setHighlightedText] = useState<string>('');
+  const [availableColors, setAvailableColors] = useState<string[]>(
+    COLORS.slice()
+  );
+  const [activeSearchTag, setActiveSearchTag] = useState<string>('');
 
-  function handlePageSize(num: number) {
+  const handlePageSize = useCallback((num: number) => {
     setPageSize(num);
     setPageNumber(0);
     const buttonPrev = document.querySelector('.page-back-btn');
     buttonPrev?.classList.remove('active');
-  }
-
-  const [fetchedFeedback, setFetchedFeedback] =
-    useState<FeedbackListProps | null>(null);
+  }, []);
 
   useEffect(() => {
     const url = `https://frontend-challenge.birdie.workers.dev/feedback?pageSize=${pageSize}&page=${pageNumber}&search=${search}`;
@@ -74,55 +85,46 @@ function App() {
     }
   }, [fetchedFeedback, pageSize]);
 
-  function handleNextPage() {
+  const handleNextPage = useCallback(() => {
     if (
       fetchedFeedback?.nextPage &&
       Math.ceil(fetchedFeedback.count / pageSize) - fetchedFeedback.nextPage > 1
     ) {
       setPageNumber((prevPage) => prevPage + 1);
     }
-  }
+  }, [fetchedFeedback, pageSize]);
 
-  function handlePreviousPage() {
+  const handlePreviousPage = useCallback(() => {
     if (fetchedFeedback && fetchedFeedback.previousPage !== null) {
       setPageNumber((prevPage) => prevPage - 1);
     }
-  }
+  }, [fetchedFeedback]);
 
-  function handleSearch(inputValue: string) {
+  const handleSearch = useCallback((inputValue: string) => {
     setSearch(inputValue);
     setPageNumber(0);
-  }
+  }, []);
 
-  function handleSearchByTag(inputValue: string, id: string) {
-    if (activeSearchTag === '') {
-      setSearch(inputValue);
-      setPageNumber(0);
-      setActiveSearchTag(id);
-    } else if (activeSearchTag === id) {
-      setSearch('');
-      setPageNumber(0);
-      setActiveSearchTag('');
-    } else {
-      setSearch(inputValue);
-      setPageNumber(0);
-      setActiveSearchTag(id);
-    }
-  }
-
-  const [tags, setTags] = useState<TagProp[]>([]);
-  const [buttonVisible, setButtonVisible] = useState<boolean>(false);
-  const [buttonPosition, setButtonPosition] = useState<{
-    top: number;
-    left: number;
-  }>({ top: 0, left: 0 });
-  const [highlightedText, setHighlightedText] = useState<string>('');
-  const [availableColors, setAvailableColors] = useState<string[]>(
-    COLORS.slice()
+  const handleSearchByTag = useCallback(
+    (inputValue: string, id: string) => {
+      if (activeSearchTag === '') {
+        setSearch(inputValue);
+        setPageNumber(0);
+        setActiveSearchTag(id);
+      } else if (activeSearchTag === id) {
+        setSearch('');
+        setPageNumber(0);
+        setActiveSearchTag('');
+      } else {
+        setSearch(inputValue);
+        setPageNumber(0);
+        setActiveSearchTag(id);
+      }
+    },
+    [activeSearchTag]
   );
-  const [activeSearchTag, setActiveSearchTag] = useState<string>('');
 
-  function handleAddTag() {
+  const handleAddTag = useCallback(() => {
     const existingTag = tags.find((tag) => tag.text === highlightedText);
 
     if (existingTag) {
@@ -144,18 +146,20 @@ function App() {
     const newTagsArr = [...tags, newTag];
     setTags(newTagsArr);
     setButtonVisible(false);
-  }
+  }, [availableColors, highlightedText, tags]);
 
-  function handleDeleteTag(id: string) {
-    const updatedTags = tags.filter((tag) => tag.id !== id);
-    setTags(updatedTags);
+  const handleDeleteTag = useCallback(
+    (id: string) => {
+      const updatedTags = tags.filter((tag) => tag.id !== id);
+      setTags(updatedTags);
 
-    // If a tag is deleted, its color can be reused
-    const deletedTag = tags.find((tag) => tag.id === id);
-    if (deletedTag) {
-      setAvailableColors((prevColors) => [...prevColors, deletedTag.color]);
-    }
-  }
+      const deletedTag = tags.find((tag) => tag.id === id);
+      if (deletedTag) {
+        setAvailableColors((prevColors) => [...prevColors, deletedTag.color]);
+      }
+    },
+    [tags]
+  );
 
   useEffect(() => {
     const handleMouse = () => {
@@ -164,9 +168,6 @@ function App() {
       if (selection && selection.rangeCount > 0 && !selection.isCollapsed) {
         const range = selection.getRangeAt(0);
         const rect = range.getBoundingClientRect();
-
-        console.log(range);
-
         if (
           range.commonAncestorContainer.parentElement?.className ===
           'feedback-text'
@@ -181,12 +182,11 @@ function App() {
       } else {
         setButtonVisible(false);
       }
-
-      return () => {
-        document.removeEventListener('mouseup', handleMouse);
-      };
     };
     document.addEventListener('mouseup', handleMouse);
+    return () => {
+      document.removeEventListener('mouseup', handleMouse);
+    };
   }, []);
 
   return (
